1c098c81897879bf8712ac360ee1a8a908fc7f1d
cuong190622 <87683673+cuong190622@users.noreply.github.com>
2021-08-28 19:59:26 +0700

    done

--- databaseHandler.js
+++ databaseHandler.js
@@ -1,27 +1,11 @@
 const {ObjectId,MongoClient} = require('mongodb');
 //const url ="mongodb://localhost:27017" ;
 const url= "mongodb+srv://new-cuongdinh-06:cuong2001@cluster0.k6asp.mongodb.net/test"
-async function insertfuntion(toys) {
-    const dbo = await getDB();
-    const newS = await dbo.collection("toys").insertOne(toys);
-    
-    console.log("gia trị id mơi dc insert la", newS.insertedId.toHexString());
-}
 
-async function Deletefuntion(idInput) {
-    const dbo = await getDB();
-    await dbo.collection("toys").deleteOne({ _id: ObjectId(idInput) });
-}
-
-async function Search(SearchInput) {
-    const dbo = await getDB();
-    const allToys = await dbo.collection("toys").find({ name: SearchInput}).toArray();
-    return allToys;
-}
-
-async function getToyById(idInput){
-    const dbo = await getDB();
-    return dbo.collection("toys").findOne({_id: ObjectId(idInput) });
+async function getDB() {
+    const client = await MongoClient.connect(url);
+    const dbo = client.db("GCH0803DB");
+    return dbo;
 }
 
 async function Showall() {
@@ -30,15 +14,38 @@ async function Showall() {
     return allToys;
 }
 
+async function insertfuntion(toys) {
+    const dbo = await getDB();
+    const newS = await dbo.collection("toys").insertOne(toys);
+    
+    console.log("gia trị id mơi dc insert la", newS.insertedId.toHexString());
+}
+
+
+
+async function Search(SearchInput) {
+    const dbo = await getDB();
+    const allToys = await dbo.collection("toys").find({ name: SearchInput}).toArray();
+    return allToys;
+}
+
+async function Deletefuntion(idInput) {
+    const dbo = await getDB();
+    await dbo.collection("toys").deleteOne({ _id: ObjectId(idInput) });
+}
+
+async function getToyById(idInput){
+    const dbo = await getDB();
+    return dbo.collection("toys").findOne({_id: ObjectId(idInput) });
+}
 async function update(id,nameInput,priceInput,imageInput){
     const dbo = await getDB();
     dbo.collection("toys").updateOne({_id:ObjectId(id)},{$set:{name:nameInput,price:priceInput,image:imageInput}})
 
 }
 
-async function getDB() {
-    const client = await MongoClient.connect(url);
-    const dbo = client.db("GCH0803DB");
-    return dbo;
-}
+
+
+
+
 module.exports = {getToyById,getDB,insertfuntion,Deletefuntion,Search,Showall,update}
